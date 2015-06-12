5f9c25b882f684cc959c6e12683297bb1ee01fb4
Anish Hallan <anishhallan@gmail.com>
2015-06-12 17:22:10 +0100

    finishing the assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,23 @@
  require 'faker'
  
  # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
+ # 50.times do
+ #   Post.create!(
+ #     title:  Faker::Lorem.sentence,
+ #     body:   Faker::Lorem.paragraph
+ #   )
+ # end
+ # posts = Post.all
+ # # Create Comments
+ # 100.times do
+ #   Comment.create!(
+ #     post: posts.sample,
+ #     body: Faker::Lorem.paragraph
+ #   )
+ # end
  
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
-
-  Post.create!(
-     title: 'Assignment',
-     body: 'Welcome to this film'
-    )
-
-  Comment.create!(
-     post_id: 5,
-     body: 'review'
-    )
-
- 
+ post = Post.find_or_create_by(title:'Assignment',body:'Welcome to this film')
+ comment = Comment.find_or_create_by(post:post,body:'review')
  puts "Seed finished"
  puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+ puts "#{Comment.count} comments created" 
