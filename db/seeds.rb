f1b9ea7434a085a563c36181a05c7faea7deff2c
Anish Hallan <anishhallan@gmail.com>
2015-06-19 16:28:37 +0100

    assignment-27 so far

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,13 @@
  
  # Create Posts
  50.times do
-   Post.create!(
+   Advertisement.create!(
      title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
+     copy:   Faker::Lorem.paragraph,
+     price:  Faker::Commerce.price
    )
  end
- posts = Post.all
  
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
  
  puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+ puts "#{Advertisement.count} advertisements created"
