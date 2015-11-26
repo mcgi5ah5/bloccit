43601680d2843e41227a4dacae1b140539f906f6
Anish Hallan <anishhallan@gmail.com>
2015-11-26 20:27:32 +0000

    Completed assignment_54

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -6,10 +6,11 @@ require 'rails_helper'
  
    before do
      @user = authenticated_user
-      @post = associated_post(user: @user)
-      @comment = Comment.new(user: @user, post: @post, body: "A Comment")
-      allow(@comment).to receive(:send_favorite_emails)
-      @comment.save!
+     @topic = Topic.create(name: "A topic", description: "A topic")
+     @post = associated_post(user: @user, topic: @topic)
+     @comment = Comment.new(user: @user, post: @post, body: "A Comment")
+     allow(@comment).to receive(:send_favorite_emails)
+     @comment.save!
    end
  
    describe "not signed in" do
@@ -22,6 +23,19 @@ require 'rails_helper'
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
      end
+
+    describe "signed in" do
+      before do
+        #user sign in using Warden
+        login_as(@user, scope: :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+    end
+
+  end
  
    end
  end
